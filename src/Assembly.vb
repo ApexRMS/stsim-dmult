47ccdb4effa77d8d196cd0ffa272a892e0ce1c50
alex <alexe_webmail@yahoo.com>
2019-02-01 17:09:10 -0800

    Update file header blocks to match readme.md.

--- src/Assembly.vb
+++ src/Assembly.vb
@@ -1,8 +1,8 @@
-﻿'*********************************************************************************************
-' Dynamic Multipliers: A SyncroSim Package of dynamic multipliers for ST-Sim
+﻿'***********************************************************************************************
+' stsim-dmult: SyncroSim Add-On Package (to stsim) for generating dynamic multipliers in ST-Sim.
 '
 ' Copyright © 2007-2019 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 '
-'*********************************************************************************************
+'***********************************************************************************************
 
 <Assembly: CLSCompliant(True)> 
--- src/DynMultTransformer.vb
+++ src/DynMultTransformer.vb
@@ -1,9 +1,9 @@
-﻿'*********************************************************************************************
-' Dynamic Multipliers: A SyncroSim Package of dynamic multipliers for ST-Sim
+﻿'***********************************************************************************************
+' stsim-dmult: SyncroSim Add-On Package (to stsim) for generating dynamic multipliers in ST-Sim.
 '
 ' Copyright © 2007-2019 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 '
-'*********************************************************************************************
+'***********************************************************************************************
 
 Imports System.IO
 Imports System.Reflection
